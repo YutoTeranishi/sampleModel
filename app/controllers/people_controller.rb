c2e49a9d900e3d934b009d1deba66180943858df
Yuto Teranishi <yt05271992@yahoo.co.jp>
2022-02-28 10:51:24 +0900

    form_helperを使った記述に変更

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -9,15 +9,25 @@ class PeopleController < ApplicationController
   end
   def add
     @msg = "add new data."
+    @person = Person.new
   end
   def create
     if request.post? then
+      Person.create(person_params)
+
+=begin [行の先頭にしないと機能しない]
       obj = Person.create(
         name: params['name'],
         age:params['age'],
         mail:params['mail']
       )
+=end
     end
     redirect_to '/people'
   end
+
+  private def person_params
+    params.require(:person).permit(:name,:age,:mail)
+  end
+
 end
