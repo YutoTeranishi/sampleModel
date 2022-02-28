c81c7b172792c0319245f47702830dffc75fb752
Yuto Teranishi <yt05271992@yahoo.co.jp>
2022-02-28 11:53:11 +0900

    update,deleteの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'people', to: 'people#index'
   get 'people/add'
   post 'people/add', to: 'people#create'
+  get 'people/edit/:id' , to: 'people#edit'
+  patch 'people/edit/:id', to: 'people#update'
+  get 'people/delete/:id' , to: 'people#delete'
+
   get 'people/:id',to:'people#show'
 
 end
