9511343c3022c09a2af079abe95d6c9ae4e3f300
Yuto Teranishi <yt05271992@yahoo.co.jp>
2022-03-01 11:03:16 +0900

    ルーティング順番変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'people/index'
   get 'people', to: 'people#index'
+  get 'people/index'
   get 'people/add'
   post 'people/add', to: 'people#create'
   get 'people/edit/:id' , to: 'people#edit'
