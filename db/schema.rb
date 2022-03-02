7920c24ee1226736387812e2c6240e610a85d4d1
Yuto Teranishi <yt05271992@yahoo.co.jp>
2022-03-02 15:47:12 +0900

    messageページの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_25_014044) do
+ActiveRecord::Schema.define(version: 2022_03_02_051626) do
+
+  create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "person_id"
+    t.text "title"
+    t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "people", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "name"
