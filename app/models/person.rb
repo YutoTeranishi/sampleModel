5f7caa3007c8270d24d516a4072afc6c94acf789
Yuto Teranishi <yt05271992@yahoo.co.jp>
2022-03-04 14:17:28 +0900

    has_manyとbelongs_toの追記と それに合わせた変更

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ApplicationRecord
+  has_many :message
+
   validates :name, presence: { message:'は、必須項目です。'}
   validates :age, numericality: { message:'数字で入力ください。'}
   validates :mail, email: { message:'はメールアドレスではありません。'}
